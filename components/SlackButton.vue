a91e44ef2b7782ddeccff5eca8fd2ee2c4f3d3a0
Chris Scott <cyberstrike@gmail.com>
2019-01-21 17:51:28 -0500

    Home page (#4)
    * basic info
    
    * added sass and created slack button
    
    * meetup button and basic responsiveness

---
+++ components/SlackButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <a :href="content.links.slack" class="btn btn-slack">
+    <span class="d-flex justify-content-center align-items-center">
+      <slack-logo class="logo"/>
+    Join Our Slack
+    </span>
+  </a>
+</template>
+
+<script>
+  import SlackLogo from './SlackLogo'
+  export default {
+    name: "SlackButton",
+    components: {SlackLogo},
+    data: () => ({
+      content: {
+        links: {
+          slack: 'https://join.slack.com/t/fedm/shared_invite/enQtMzgyMTMzNTI5NDQ3LTdiMmRjNDA2ODE5YWI0NGFmZjg0ZGUzZTUxMDg4MmJmYWI4ZWZjZDNjZjJkYmUyMThlMDVlMzA2YWY4ZWU5YWQ'
+        }
+      }
+    })
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "~bootstrap/scss/functions";
+  @import "~bootstrap/scss/variables";
+  @import "~bootstrap/scss/mixins";
+
+  .btn-slack {
+    @include button-variant($indigo, $indigo);
+    font-weight: bold;
+    .logo {
+      height: 1rem;
+      margin-right: 0.5em;
+    }
+  }
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,27 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        www
-      </h1>
-      <h2 class="subtitle">
-        frontend.miami site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+    <div class="row vh-100 align-items-center justify-content-center">
+      <div class="col-10 mx-auto">
+        <div class="row">
+          <div class="col-12 col-lg-6 text-center">
+            <logo style="height: 100%; width: 100%"/>
+          </div>
+          <div class="col-12 col-lg-6">
+            <h1 class="title">{{content.title}}</h1>
+            <div class="my-3">
+              <meetup-button/>
+              <slack-button/>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <div class="card-text">
+                  <div v-html="content.description">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -26,43 +29,34 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+import SlackButton from '../components/SlackButton'
+import MeetupButton from '../components/MeetupButton'
 
 export default {
   components: {
+    MeetupButton,
+    SlackButton,
     Logo
-  }
+  },
+  data: () => ({
+    content: {
+      title: 'Front-End Miami',
+      description: `<p>
+        Front-End Developers of Miami is a MeetUp group that tries to have monthly
+        gatherings about topics relevant to web development and front-end engineering.
+      </p>
+      <p>
+        Venues have included The LAB Miami and O Cinema Wynwood, both in Wynwood.
+        Topics include, but are certainly not limited to the following: responsive design using CSS, HTML5,
+        client side programming and practices with Javascript as well as the tools
+        and practices to keep everything in its place.
+      </p><p>
+        We are always looking for sponsors (for food & beverages)as well as speakers for our gatherings.
+      </p>`
+    }
+  })
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
