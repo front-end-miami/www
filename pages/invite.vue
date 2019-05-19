27745558c93732b5df37c54c1cacb75408e8767e
Chris Scott <cyberstrike@gmail.com>
2019-05-19 18:52:09 -0400

    added new invite redirect rout

---
+++ pages/invite.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container">
+    <div class="row vh-100 align-items-center justify-content-center">
+      <div class="col-12 mx-auto">
+        <div style="width:100%;height:0;padding-bottom:56%;position:relative;">
+          <iframe src="https://giphy.com/embed/l4JyOCNEfXvVYEqB2" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        </div>
+        <h3 class="text-center">Redirecting to Slack Invite in {{timerLength / 100}}</h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "invite",
+    data: () => ({
+      intervalRef: null, // A reference for the set timer so we can clear it.
+      timerLength: 3000,
+      timerInterval: 100,
+      redirectUrl: "https://join.slack.com/t/fedm/shared_invite/enQtMzgyMTMzNTI5NDQ3LTdiMmRjNDA2ODE5YWI0NGFmZjg0ZGUzZTUxMDg4MmJmYWI4ZWZjZDNjZjJkYmUyMThlMDVlMzA2YWY4ZWU5YWQ"
+    }),
+    methods: {
+      setTime () {
+        // EW mutations..
+        this.timerLength = this.timerLength - this.timerInterval
+
+        // We could use a watch for this but since we are already
+        // calling this with every change there is no need.
+        if (0 >= this.timerLength) {
+          this.clearTimer() // Always clear your interval timers
+          this.redirect();
+        }
+      },
+      startTimer () {
+        this.intervalRef = setInterval(() => this.setTime(), this.timerInterval);
+      },
+      clearTimer () {
+        clearInterval(this.intervalRef)
+      },
+      redirect () {
+        window.location.replace(this.redirectUrl)
+      },
+    },
+    mounted () { this.startTimer() }
+  }
+</script>
