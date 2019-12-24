c7aa8d91c6a7dfe15815f8ace8e738a76a273339
Charles Villard <c.d.villard@gmail.com>
2019-12-24 16:39:20 -0500

    Fixed typo

--- components/NextMeetup.vue
+++ components/NextMeetup.vue
@@ -21,7 +21,7 @@
      <div class="card alert-warning mb-3">
        <div class="card-body">
          <p class="card-text">
-           ⚠️ They are no events scheduled at this time, or we an experiencing an error with the api.
+           ⚠️ There are no events scheduled at this time, or we an experiencing an error with the api.
            You can find event details on <a href="https://www.meetup.com/Front-end-Developers-of-Miami/">our meetup page</a>.
          </p>
        </div>
