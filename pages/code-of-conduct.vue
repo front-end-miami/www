af84f8b167dc0c00abf6d5ee7ede85d29fe7bc8c
Chris Scott <cyberstrike@gmail.com>
2021-01-12 12:31:33 -0500

    [Feature] Code of conduct Page (#23)
    * added markdown-it package
    
    * added code of conduct markdown file
    
    * added coc page and styling
    
    * added logo to coc page
    
    * misc styling
    
    * Update code-of-conduct.md
    
    * [Bugfix] No Meetup (#19)
    
    * fixed no meetup bug, updated no meetup text
    
    Co-authored-by: Charles Villard <c.d.villard@gmail.com>
    
    * ✨ NEW: Add Logo
    
    * Bump acorn from 6.3.0 to 6.4.1 (#24)
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 6.3.0 to 6.4.1.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/6.3.0...6.4.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * 🐣 NEW: Donor Box Logo
    
    * 🐣 NEW: Donate Button
    
    * ✨ ADD: Donor Button Index
    
    * 📦 PKG: Update Markdown It
    
    * ✨ ADD: Link to coc on index and copy year
    
    Co-authored-by: Charles Villard <c.d.villard@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col mx-auto my-5 text-center">
+        <nuxt-link to="/">
+          <logo size="250px"/>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-lg-6 mx-auto">
+        <div class="card">
+          <div class="card-body">
+            <div v-html="coc"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row py-5">
+      <div class="col text-center">
+        &copy; {{ copyYear }} Front-End Miami &bullet; <NuxtLink to="/">Home</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Logo from '~/components/Logo'
+  import coc from '~/assets/code-of-conduct.md'
+
+  export default {
+    name: 'code-of-conduct',
+    asyncData: () => ({ coc }),
+    components: { Logo }
+  }
+</script>
