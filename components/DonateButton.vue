3fb9867ca5db34c022c8154856be21be7ad56ca0
Chris Scott <cyberstrike@gmail.com>
2021-01-11 21:00:06 -0500

    🐣 NEW: Donate Button

---
+++ components/DonateButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="$options.static.link" class="btn btn-info font-weight-bolder">
+    <span class="d-flex justify-content-center align-items-center">
+      <donor-box-logo class="logo"/> donorbox
+    </span>
+  </a>
+</template>
+
+<script>
+  import DonorBoxLogo from './DonorBoxLogo'
+
+  export default {
+    name: 'DonateButton',
+    static: {
+      link: 'https://donorbox.org/front-end-miami-developer-community'
+    },
+    components: { DonorBoxLogo }
+  }
+</script>
+
+<style scoped>
+  .logo {
+    height: 1rem;
+    margin-right: 0.5em;
+  }
+</style>
