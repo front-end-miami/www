2cbb30036c8e6ecabe316281ade51813691d6fda
Chris Scott <cyberstrike@gmail.com>
2021-08-19 04:17:44 -0400

    📦 PKG: Prettier + Eslint + Standard Installed

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['@vue/standard', 'eslint:recommended', 'plugin:vue/recommended'],
+  rules: {
+    indent: ['error', 2],
+    'vue/script-indent': ['error', 2, { baseIndent: 1 }], // Ident script tags
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off'
+      }
+    }
+  ]
+}
